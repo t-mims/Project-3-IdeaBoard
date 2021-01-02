4c425e20fa1aede184b12adce06c528f94f1b44c
Gina Dannibale <ginadannibale@gmail.com>
2021-01-02 11:52:12 -0600

    change

---
+++ collections/index.js
@@ -0,0 +1,4 @@
+module.exports = {
+    Users: require("./users"),
+    Images: require("./images")
+  };
--- server.js
+++ server.js
@@ -3,6 +3,12 @@ const mongoose = require("mongoose");
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+const db = require("./models");
+
+const app = express();
+
+app.use(logger("dev"));
+
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());

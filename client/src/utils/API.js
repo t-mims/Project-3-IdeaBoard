df829d8799ae83208c5339e3e7b026b00597a93a
Tahliyah Mims <mimst97@gmail.com>
2021-01-04 20:18:58 -0600

    more file relocation

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-//for images api
+//for unsplash images api
 //key 1 is access key, key2 is security key for unsplash API
 const accessKey= "0fOPsIZ3x5wzU0_f5CoDIvuk6Bf3IHmpTzrpix9EINM";
 const randomURL="https://api.unsplash.com/photos/random/?count=30";
@@ -11,5 +11,4 @@ export default{
     getUserSearch: function(query){
         return axios.get(queryURL+ "?"+query +"&client_id="+accessKey);
     }
-}
-https://api.unsplash.com/photos/?client_id=0fOPsIZ3x5wzU0_f5CoDIvuk6Bf3IHmpTzrpix9EINM
+};

a2fbff80e1536fafa0f7e04731a94460b5eb333a
Tahliyah Mims <mimst97@gmail.com>
2021-01-04 18:21:37 -0600

    rm extra folder, fixed unsplash urls

--- src/utils/API.js
+++ src/utils/API.js
@@ -1,15 +1,15 @@
 import axios from "axios";
 //for images api
 //key 1 is access key, key2 is security key for unsplash API
-const key1= "0fOPsIZ3x5wzU0_f5CoDIvuk6Bf3IHmpTzrpix9EINM";
-const key2="bRb0d6z50WNoYe1anJu6GNY7OxsCtIuVlDpF3otQFZ4";
-const randomURL="https://api.unsplash.com/GET/photos/random/count=30";
-const queryURL="https://api.unsplash.com/GET/"; 
+const accessKey= "0fOPsIZ3x5wzU0_f5CoDIvuk6Bf3IHmpTzrpix9EINM";
+const randomURL="https://api.unsplash.com/photos/random/?count=30";
+const queryURL="https://api.unsplash.com/"; 
 export default{
     getRandomImages: function(){
-        return axios.get(randomURL+key);
+        return axios.get(randomURL+"&client_id="+accessKey);
     },
     getUserSearch: function(query){
-        return axios.get(queryURL+ query +key);
+        return axios.get(queryURL+ "?"+query +"&client_id="+accessKey);
     }
 }
+https://api.unsplash.com/photos/?client_id=0fOPsIZ3x5wzU0_f5CoDIvuk6Bf3IHmpTzrpix9EINM

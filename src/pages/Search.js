66643e3d3bb15e92b07285229d80005689d25f2d
Tahliyah Mims <mimst97@gmail.com>
2021-01-09 10:35:45 -0600

    pseudocoding/notes

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -2,7 +2,12 @@ import React from "react";
 import Container from "../components/container"
 // will require integration of state as well as handleSumbits for that will call Unsplash
 // may want to initiate (componentDidMount) with display of random results
-
+//-----------------------------------------
+//class Search extends Component{
+//state={ search:"",results:[],error:""}
+//componentDidMount(){API.getRandomImages()
+//.then(res=>this.setState({results:res.data----Plug in ref to random images res}))}
+//====Maybe overlay the search bar over the random images in the background??
 function Search(){
     return (
         <div>This is for the actual search input text thing 

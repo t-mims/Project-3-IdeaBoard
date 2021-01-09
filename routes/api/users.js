3ab668f07c34c1258eb97bb9bf179332a7454af2
Gina Dannibale <ginadannibale@gmail.com>
2021-01-09 12:43:57 -0600

    change

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,7 +1,25 @@
+
+const express = require('express');
 const router= require("express").Router();
 const userController= require("../../controllers/userController");
 //this file will utilize the controller and select methods based on request
 //route will be "/api/user" + name in router 
+router.get('/login',(req,res)=>{
+    res.render('login');
+})
+router.get('/register',(req,res)=>{
+    res.render('register')
+    })
+//Register handle
+router.post('/register',(req,res)=>{
+})
+router.post('/login',(req,res,next)=>{
+  })
+
+//logout
+router.get('/logout',(req,res)=>{
+ })
+
 router.route("/")
     .get(userController.findUser)
     .post(userController.createUser)

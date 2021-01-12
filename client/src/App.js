7b18238b32b89674f856d409643f4061c363cd39
Tahliyah Mims <mimst97@gmail.com>
2021-01-11 20:54:33 -0600

    Fixed passport location

--- client/src/App.js
+++ client/src/App.js
@@ -7,28 +7,6 @@ import myBoard from "./pages/myBoard";
 import Search from "./pages/Search";
 import Home from "./pages/Home";
 
-const express = require('express');
-const router = express.Router();
-const app = express();
-const mongoose = require('mongoose');
-const expressEjsLayout = require('express-ejs-layouts')
-
-//mongoose
-mongoose.connect('mongodb://localhost/test',{useNewUrlParser: true, useUnifiedTopology : true})
-.then(() => console.log('connected,,'))
-.catch((err)=> console.log(err));
-//EJS
-// app.set('view engine','ejs');
-// app.use(expressEjsLayout);
-//BodyParser
-app.use(express.urlencoded({extended : false}));
-
-//Routes
-app.use('/',require('../routes/api/index'));
-app.use('/users',require('../routes/api/users'));
-
-app.listen(3000);
-
 //and import other necessary pages (which will correspond to the routes) and compnents
 function App() {
   return (

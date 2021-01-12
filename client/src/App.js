744239d7297cac76a8b35bf956ab931ad9235907
Tahliyah Mims <mimst97@gmail.com>
2021-01-11 19:57:23 -0600

    Client Folder! (sep packageJSON for FE and BE)

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,3 @@
-const express = require('express');
-const router = express.Router();
-const app = express();
-const mongoose = require('mongoose');
-const expressEjsLayout = require('express-ejs-layouts')
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Navbar from "./components/navbar";
@@ -11,19 +6,26 @@ import Inspo from  "./pages/Inspo";
 import myBoard from "./pages/myBoard";
 import Search from "./pages/Search";
 import Home from "./pages/Home";
+
+const express = require('express');
+const router = express.Router();
+const app = express();
+const mongoose = require('mongoose');
+const expressEjsLayout = require('express-ejs-layouts')
+
 //mongoose
 mongoose.connect('mongodb://localhost/test',{useNewUrlParser: true, useUnifiedTopology : true})
 .then(() => console.log('connected,,'))
 .catch((err)=> console.log(err));
 //EJS
-app.set('view engine','ejs');
-app.use(expressEjsLayout);
+// app.set('view engine','ejs');
+// app.use(expressEjsLayout);
 //BodyParser
 app.use(express.urlencoded({extended : false}));
 
 //Routes
-app.use('/',require('./routes/index'));
-app.use('/users',require('./routes/users'));
+app.use('/',require('../routes/api/index'));
+app.use('/users',require('../routes/api/users'));
 
 app.listen(3000);
 

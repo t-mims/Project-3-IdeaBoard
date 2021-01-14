abecf45dbb867fa61dc2caa35d1ed810d284aca8
Tahliyah Mims <mimst97@gmail.com>
2021-01-13 21:21:52 -0600

    moving html to return in home page

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -4,14 +4,18 @@ import Container from "../components/container"
 
 //This is a static page/ may include state in order to greet user by their name but
 // definetly not a necessary function
-function Home(){
+function Home() {
     return (
-   
+
         <div>This is home for the home page
-        <Container>
-         <image src=""></image>
+            <Container>
+                <image src=""></image>
+                <h1>Welcome!</h1>
+                <p>Create an account or login</p>
+                <a href="/user/register">Register</a>
+                <a href="/user/login"> Login</a>
           </Container>
-        </div>
+        </div >
     )
 }
 export default Home

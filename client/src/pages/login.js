58c703a1aa9c8d780a972f668ed49d373e9267d8
Gina Dannibale <ginadannibale@gmail.com>
2021-01-13 20:35:40 -0600

    change

--- client/src/pages/login.js
+++ client/src/pages/login.js
@@ -1,16 +1,26 @@
 import React from "react";
-import Container from "../components/container"
+import Container from "../components/container";
 
 //This is a static page/ may include state in order to greet user by their name but
 // definetly not a necessary function
-function Login(){
-    // return (
-   
-    //     <div>This is home for the home page
-    //     <Container>
-    //      <image src=""></image>
-    //       </Container>
-    //     </div>
-    // )
+function Login() {
+    return (
+        <div>
+  <form action="/login" method="post">
+    <div>
+      <label>Username:</label>
+      <input type="text" name="username" />
+    </div>
+    <div>
+      <label>Password:</label>
+      <input type="password" name="password" />
+    </div>
+    <div>
+      <input type="submit" value="Log In" />
+    </div>
+  </form>
+  </div>
+
+    )
 }
-export default Login
+export default Login;
--- server.js
+++ server.js
@@ -1,27 +1,10 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const passport= require("passport");
+const passport = require("./config/passport");
 const routes = require("./routes");
 const app = express();
 const PORT = process.env.PORT || 3001;
-const db = require("./board");
-
-LocalStrategy = require('passport-local').Strategy;
-
-passport.use(new LocalStrategy(
-  function(username, password, done) {
-    User.findOne({ username: username }, function (err, user) {
-      if (err) { return done(err); }
-      if (!user) {
-        return done(null, false, { message: 'Incorrect username.' });
-      }
-      if (!user.validPassword(password)) {
-        return done(null, false, { message: 'Incorrect password.' });
-      }
-      return done(null, user);
-    });
-  }
-));
+const db = require("./models");
 
 app.configure(function() {
   app.use(express.static('public'));
@@ -46,7 +29,7 @@ app.post('/login',
   function(req, res) {
     // If this function gets called, authentication was successful.
     // `req.user` contains the authenticated user.
-    res.redirect('./models/user/' + req.user.username);
+    res.redirect('/' + req.user.username);
   });                                       
 
 // Connect to the Mongo DB
@@ -54,7 +37,7 @@ mongoose.connect('mongodb://localhost/test',{useNewUrlParser: true, useUnifiedTo
 .then(() => console.log('connected,,'))
 .catch((err)=> console.log(err));
 
-db.board.create({ name: "" })
+db.Board.create({ name: "" })
   .then(dbboard => {
     console.log(dbboard);
   })

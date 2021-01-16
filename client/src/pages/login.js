fb2e6d8b83a9234a51b2b4455868ad89d01bf045
Gina Dannibale <ginadannibale@gmail.com>
2021-01-16 13:38:17 -0600

    passport

--- client/src/pages/login.js
+++ client/src/pages/login.js
@@ -1,26 +1,66 @@
-import React from "react";
+import React, { useState } from "react";
 import Container from "../components/container";
+import API from "../utils/API"
 
 //This is a static page/ may include state in order to greet user by their name but
 // definetly not a necessary function
 function Login() {
-    return (
-        <div>
-  <form action="/login" method="post">
+  const [username, setUsername] = useState();
+  const [password, setPassword] = useState();
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    API.login({ username: username, password: password })
+      .then((res) => {
+        console.log("login");
+        //redirect to Board page
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+  return (
     <div>
-      <label>Username:</label>
-      <input type="text" name="username" />
+      This is home for the home page
+      <Container>
+        <h1>Login</h1>
+        <form>
+          <div>
+            <label for="name">Name</label>
+            <input
+              type="name"
+              name="name"
+              placeholder="Enter Name"
+              onChange={(event) => setUsername(event.target.value)}
+            />
+          </div>
+          <div>
+            <label for="password">Password</label>
+            <input
+              type="password"
+              name="password"
+              placeholder="Create Password"
+              onChange={(event) => setPassword(event.target.value)}
+            />
+          </div>
+          <button
+            type="submit"
+            className="btn btn-primary btn-block"
+            onClick={handleSubmit}
+          >
+            Login
+          </button>
+        </form>
+      </Container>
     </div>
-    <div>
-      <label>Password:</label>
-      <input type="password" name="password" />
-    </div>
-    <div>
-      <input type="submit" value="Log In" />
-    </div>
-  </form>
-  </div>
-
-    )
+  );
 }
+// return (
+
+//     <div>This is home for the home page
+//     <Container>
+//      <image src=""></image>
+//       </Container>
+//     </div>
+// )
+
 export default Login;

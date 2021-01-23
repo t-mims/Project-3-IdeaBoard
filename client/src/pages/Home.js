418d12e385a6894d8f38a7c89641d63ed51cdae1
Gina Dannibale <ginadannibale@gmail.com>
2021-01-22 21:03:14 -0600

    picture

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,16 +1,18 @@
 import React from "react";
-import Container from "../components/container"
+import Container from "../components/container";
 
 //This is a static page/ may include state in order to greet user by their name but
 // definetly not a necessary function
-function Home(){
-    return (
-   
-        <div>Dream big, dream often, dream wild.
-        <Container>
-         <image src=""></image>
-          </Container>
+function Home() {
+  return (
+    <div>
+      <Container>
+        <div>
+        <img src="https://previews.123rf.com/images/emilivanov/emilivanov1803/emilivanov180300060/97391734-photo-collage-of-bulgarian-travel-photos-grid-of-landmarks-and-nature-scenes-from-bulgaria.jpg" float center></img>
+
         </div>
-    )
+      </Container>
+    </div>
+  );
 }
-export default Home
+export default Home;

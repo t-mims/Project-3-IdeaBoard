1bc5de2a3ef8d7fd34d0bb19c0a296210643d8d7
Tahliyah Mims <mimst97@gmail.com>
2021-01-22 22:27:24 -0600

    another heroku fix

--- server.js
+++ server.js
@@ -19,6 +19,9 @@ app.use(passport.session());
 // Serve up static assets (usually on heroku) 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
+  // app.get("*", (req, res) => {
+  //   res.sendFile(path.resolve(__dirname, "build", "index.html"));
+  // });
 }
 
 require("./routes/api/api-routes")(app);

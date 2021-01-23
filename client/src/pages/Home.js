005a9702aad653f50211225e262d51625e75cc01
Tahliyah Mims <mimst97@gmail.com>
2021-01-23 08:48:32 -0600

    rm extra closing tag

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -3,15 +3,13 @@ import Container from "../components/container";
 
 //This is a static page/ may include state in order to greet user by their name but
 // definetly not a necessary function
-function Home(){
-    return (
-   
-        <div>
-        <Container>
-            <h3>Dream big, dream often, dream wild.</h3>
-        </div>
+function Home() {
+  return (
+    <div>
+      <Container>
+        <h3>Dream big, dream often, dream wild.</h3>
       </Container>
-    </div>
+    </div >
   );
 }
 export default Home;

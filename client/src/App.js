d367e393fe3d70cd3e85b56cf376519a07aec66a
Tahliyah Mims <mimst97@gmail.com>
2021-01-22 23:35:07 -0600

    useHistory change

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Route , useHistory} from "react-router-dom";
+import {useHistory, BrowserRouter as Router, Route} from "react-router-dom";
 import Navbar from "./components/navbar";
 import Footer from "./components/footer";
 import NewBoard from "./pages/newBoard";

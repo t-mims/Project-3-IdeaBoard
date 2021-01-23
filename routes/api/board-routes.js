dfaedb0e7da34fddeb1f8767d3a1f8b707e20270
Tahliyah Mims <mimst97@gmail.com>
2021-01-22 20:26:58 -0600

    Trying to fix heroku

--- routes/api/board-routes.js
+++
@@ -1,7 +0,0 @@
-var router = require("express").Router()
-var boardController = require("../../controllers/boardController")
-
-router.route("/")
-      .get(boardController.getBoards).post(boardController.createBoard)
-      
-      module.exports = router
--- server.js
+++ server.js
@@ -16,8 +16,13 @@ const session = require("express-session")
   // app.use(app.router);
 
 // Serve up static assets (usually on heroku)
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static("client/build"));
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
+  app.use(express.static("build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname,  "build", "index.html"));
+  });
 }
 
  require("./routes/api/api-routes")(app);  
